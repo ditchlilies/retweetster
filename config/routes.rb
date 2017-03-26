7e052b19e3cf55ce57753ee9e394ca0e159ae32b
Elliot Patterson <sarah.elliot.rhodes@gmail.com>
2017-03-26 00:52:39 +0000

    db migrated

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'tweets/index'
+
+  get 'tweets/show'
+
+  get 'tweets/new'
+
+  get 'tweets/edit'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
